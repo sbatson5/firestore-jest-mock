1926c2c9328a95f8466d85d6d8841b154e8be592
James Robinson <1500092+AverageHelper@users.noreply.github.com>
2020-08-05 13:46:42 -0600

    Add getUser and setCustomUserClaims mocks (#36)
    * Add getUser and setCustomUserClaims mocks
    
    * Update README
    
    * Allow mocked auth functions to inject return values.
    
    * Revert emoji 😉

--- mocks/auth.js
+++ mocks/auth.js
@@ -4,9 +4,12 @@ const mockSendVerificationEmail = jest.fn();
 const mockSignInWithEmailAndPassword = jest.fn();
 const mockSendPasswordResetEmail = jest.fn();
 const mockVerifyIdToken = jest.fn();
+const mockGetUser = jest.fn();
+const mockSetCustomUserClaims = jest.fn();
 
 class FakeAuth {
   constructor(currentUser) {
+    currentUser = currentUser || {};
     currentUser.sendEmailVerification = mockSendVerificationEmail;
     this.currentUserRecord = currentUser;
   }
@@ -35,6 +38,14 @@ class FakeAuth {
     return Promise.resolve(this.currentUserRecord);
   }
 
+  getUser() {
+    return Promise.resolve(mockGetUser(...arguments) || {});
+  }
+
+  setCustomUserClaims() {
+    return Promise.resolve(mockSetCustomUserClaims(...arguments) || {});
+  }
+
   get currentUser() {
     const { uid, ...data } = this.currentUser;
     return { uid, data };
@@ -49,4 +60,6 @@ module.exports = {
   mockSendVerificationEmail,
   mockSignInWithEmailAndPassword,
   mockVerifyIdToken,
+  mockGetUser,
+  mockSetCustomUserClaims,
 };
