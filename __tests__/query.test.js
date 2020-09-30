e665fc346e751422bf221535a14698d42878d9ce
AverageHelper <1500092+AverageHelper@users.noreply.github.com>
2020-09-30 12:19:58 -0600

    More operations and unit-tests

--- __tests__/query.test.js
+++ __tests__/query.test.js
@@ -5,10 +5,42 @@ describe('test', () => {
   mockFirebase({
     database: {
       animals: [
-        { id: 'monkey', name: 'monkey', type: 'mammal' },
-        { id: 'elephant', name: 'elephant', type: 'mammal' },
-        { id: 'chicken', name: 'chicken', type: 'bird' },
-        { id: 'ant', name: 'ant', type: 'insect' },
+        {
+          id: 'monkey',
+          name: 'monkey',
+          type: 'mammal',
+          legCount: 2,
+          food: ['banana', 'mango'],
+          foodCount: 1,
+          foodEaten: [500, 20],
+        },
+        {
+          id: 'elephant',
+          name: 'elephant',
+          type: 'mammal',
+          legCount: 4,
+          food: ['banana', 'peanut'],
+          foodCount: 0,
+          foodEaten: [0, 500],
+        },
+        {
+          id: 'chicken',
+          name: 'chicken',
+          type: 'bird',
+          legCount: 2,
+          food: ['leaf', 'nut', 'ant'],
+          foodCount: 4,
+          foodEaten: [80, 20, 16],
+        },
+        {
+          id: 'ant',
+          name: 'ant',
+          type: 'insect',
+          legCount: 6,
+          food: ['leaf', 'bread'],
+          foodCount: 2,
+          foodEaten: [80, 12],
+        },
       ],
     },
     currentUser: { uid: 'homer-user' },
@@ -90,4 +122,134 @@ describe('test', () => {
     expect(mockGet).toHaveBeenCalled();
     expect(mockOffset).toHaveBeenCalledWith(2);
   });
+
+  /* eslint-disable indent */
+  test.each`
+    comp    | value     | count
+    ${'=='} | ${2}      | ${2}
+    ${'=='} | ${4}      | ${1}
+    ${'=='} | ${6}      | ${1}
+    ${'=='} | ${7}      | ${0}
+    ${'>'}  | ${1}      | ${4}
+    ${'>'}  | ${6}      | ${0}
+    ${'>='} | ${6}      | ${1}
+    ${'>='} | ${0}      | ${4}
+    ${'<'}  | ${2}      | ${0}
+    ${'<'}  | ${6}      | ${3}
+    ${'<='} | ${2}      | ${2}
+    ${'<='} | ${6}      | ${4}
+    ${'in'} | ${[6, 2]} | ${3}
+  `(
+    // eslint-disable-next-line quotes
+    "it performs '$comp' queries on number values ($count doc(s) where legCount $comp $value)",
+    async ({ comp, value, count }) => {
+      const results = await db
+        .collection('animals')
+        .where('legCount', comp, value)
+        .get();
+      expect(results.size).toBe(count);
+    },
+  );
+
+  test.each`
+    comp    | value     | count
+    ${'=='} | ${0}      | ${1}
+    ${'=='} | ${1}      | ${1}
+    ${'=='} | ${2}      | ${1}
+    ${'=='} | ${4}      | ${1}
+    ${'=='} | ${6}      | ${0}
+    ${'>'}  | ${-1}     | ${4}
+    ${'>'}  | ${0}      | ${3}
+    ${'>'}  | ${1}      | ${2}
+    ${'>'}  | ${4}      | ${0}
+    ${'>='} | ${6}      | ${0}
+    ${'>='} | ${4}      | ${1}
+    ${'>='} | ${0}      | ${4}
+    ${'<'}  | ${2}      | ${2}
+    ${'<'}  | ${6}      | ${4}
+    ${'<='} | ${2}      | ${3}
+    ${'<='} | ${6}      | ${4}
+    ${'in'} | ${[2, 0]} | ${2}
+  `(
+    // eslint-disable-next-line quotes
+    "it performs '$comp' queries on possibly-zero number values ($count doc(s) where foodCount $comp $value)",
+    async ({ comp, value, count }) => {
+      const results = await db
+        .collection('animals')
+        .where('foodCount', comp, value)
+        .get();
+      expect(results.size).toBe(count);
+    },
+  );
+
+  test.each`
+    comp    | value                      | count
+    ${'=='} | ${'mammal'}                | ${2}
+    ${'=='} | ${'bird'}                  | ${1}
+    ${'=='} | ${'fish'}                  | ${0}
+    ${'>'}  | ${'insect'}                | ${2}
+    ${'>'}  | ${'z'}                     | ${0}
+    ${'>='} | ${'mammal'}                | ${2}
+    ${'>='} | ${'insect'}                | ${3}
+    ${'<'}  | ${'bird'}                  | ${0}
+    ${'<'}  | ${'mammal'}                | ${2}
+    ${'<='} | ${'mammal'}                | ${4}
+    ${'<='} | ${'bird'}                  | ${1}
+    ${'<='} | ${'a'}                     | ${0}
+    ${'in'} | ${['a', 'bird', 'mammal']} | ${3}
+  `(
+    // eslint-disable-next-line quotes
+    "it performs '$comp' queries on string values ($count doc(s) where type $comp '$value')",
+    async ({ comp, value, count }) => {
+      const results = await db
+        .collection('animals')
+        .where('type', comp, value)
+        .get();
+      expect(results.size).toBe(count);
+    },
+  );
+
+  test.each`
+    comp                    | value                            | count
+    ${'=='}                 | ${['banana', 'mango']}           | ${1}
+    ${'=='}                 | ${['mango', 'banana']}           | ${0}
+    ${'=='}                 | ${['banana', 'peanut']}          | ${1}
+    ${'array-contains'}     | ${'banana'}                      | ${2}
+    ${'array-contains'}     | ${'leaf'}                        | ${2}
+    ${'array-contains'}     | ${'bread'}                       | ${1}
+    ${'array-contains-any'} | ${['banana', 'mango', 'peanut']} | ${2}
+  `(
+    // eslint-disable-next-line quotes
+    "it performs '$comp' queries on array values ($count doc(s) where food $comp '$value')",
+    async ({ comp, value, count }) => {
+      const results = await db
+        .collection('animals')
+        .where('food', comp, value)
+        .get();
+      expect(results.size).toBe(count);
+    },
+  );
+
+  test.each`
+    comp                    | value           | count
+    ${'=='}                 | ${[500, 20]}    | ${1}
+    ${'=='}                 | ${[20, 500]}    | ${0}
+    ${'=='}                 | ${[0, 500]}     | ${1}
+    ${'array-contains'}     | ${500}          | ${2}
+    ${'array-contains'}     | ${80}           | ${2}
+    ${'array-contains'}     | ${12}           | ${1}
+    ${'array-contains'}     | ${0}            | ${1}
+    ${'array-contains-any'} | ${[0, 11, 500]} | ${2}
+  `(
+    // eslint-disable-next-line quotes
+    "it performs '$comp' queries on possibly-zero array values ($count doc(s) where foodEaten $comp '$value')",
+    async ({ comp, value, count }) => {
+      const results = await db
+        .collection('animals')
+        .where('foodEaten', comp, value)
+        .get();
+      expect(results.size).toBe(count);
+    },
+  );
+  /* eslint-enable indent */
 });
--- mocks/helpers/buildQuerySnapShot.js
+++ mocks/helpers/buildQuerySnapShot.js
@@ -5,26 +5,83 @@ module.exports = function buildQuerySnapShot(requestedRecords, filters) {
   if (filters && Array.isArray(filters) && filters.length > 0) {
     // Apply filters
     filters.forEach(({ key, comp, value }) => {
-      // comp is '<' | '<=' | '==' | '>=' | '>' | 'array-contains'
-      // TODO: Add tests and docs for each of these
+      // https://firebase.google.com/docs/reference/js/firebase.firestore#wherefilterop
+      // Convert values to string to make Array comparisons work
+      // See https://jsbin.com/bibawaf/edit?js,console
+      // TODO: Experiment, figure out how Firestore reacts to `null` and other weird values
+
       switch (comp) {
+        // https://firebase.google.com/docs/firestore/query-data/queries#query_operators
         case '<':
-          results = results.filter(record => record && record[key] < value);
+          results = results.filter(record => {
+            if (!record || (!record[key] && record[key] !== 0)) {
+              return false;
+            }
+            return String(record[key]) < String(value);
+          });
           break;
         case '<=':
-          results = results.filter(record => record && record[key] <= value);
+          results = results.filter(record => {
+            if (!record || (!record[key] && record[key] !== 0)) {
+              return false;
+            }
+            return String(record[key]) <= String(value);
+          });
           break;
         case '==':
-          results = results.filter(record => record && record[key] === value);
+          results = results.filter(record => {
+            if (!record || (!record[key] && record[key] !== 0)) {
+              return false;
+            }
+            return String(record[key]) === String(value);
+          });
           break;
         case '>=':
-          results = results.filter(record => record && record[key] >= value);
+          results = results.filter(record => {
+            if (!record || (!record[key] && record[key] !== 0)) {
+              return false;
+            }
+            return String(record[key]) >= String(value);
+          });
           break;
         case '>':
-          results = results.filter(record => record && record[key] > value);
+          results = results.filter(record => {
+            if (!record || (!record[key] && record[key] !== 0)) {
+              return false;
+            }
+            return String(record[key]) > String(value);
+          });
           break;
+
+        // https://firebase.google.com/docs/firestore/query-data/queries#array_membership
         case 'array-contains':
-          results = results.filter(record => record && record[key] && record[key].includes(value));
+          results = results.filter(
+            record =>
+              record && record[key] && Array.isArray(record[key]) && record[key].includes(value),
+          );
+          break;
+
+        // https://firebase.google.com/docs/firestore/query-data/queries#in_and_array-contains-any
+        // TODO: Throw an error when a value is passed that contains more than 10 values
+        case 'in':
+          results = results.filter(record => {
+            if (!record || (!record[key] && record[key] !== 0)) {
+              return false;
+            }
+            return value && Array.isArray(value) && value.includes(record[key]);
+          });
+          break;
+
+        case 'array-contains-any':
+          results = results.filter(
+            record =>
+              record &&
+              record[key] &&
+              Array.isArray(record[key]) &&
+              value &&
+              Array.isArray(value) &&
+              record[key].some(v => value.includes(v)),
+          );
           break;
       }
     });
