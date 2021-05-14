c2e5bd1bbc24aaa51c4854aae3da20e387e6b622
Mutagen-D <mutagen-d@protonmail.com>
2021-05-14 22:09:59 +0300

    test(path): add more test to path

--- __tests__/path.test.js
+++ __tests__/path.test.js
@@ -1,6 +1,7 @@
 const { mockFirebase } = require('firestore-jest-mock');
 mockFirebase({ database: {} });
 const firebase = require('firebase');
+const path = require('../mocks/path');
 
 describe('Single values transformed by field sentinels', () => {
   test('isEqual', () => {
@@ -9,6 +10,21 @@ describe('Single values transformed by field sentinels', () => {
     expect(path1.isEqual(path1)).toBe(true);
     expect(path2.isEqual(path2)).toBe(true);
     expect(path1.isEqual(path2)).toBe(false);
+  });
+
+  test('compareTo', () => {
+    const path1 = new path.Path(['abc', 'def', 'ghij']);
+    const path2 = new path.Path(['abc', 'def', 'ghik']);
+    expect(path1.compareTo(path2)).toEqual(-1);
+    const path3 = new path.Path(['abc', 'def', 'ghi']);
+    expect(path1.compareTo(path3)).toEqual(1);
+    const path4 = new path.Path(['abc', 'def']);
+    const path5 = new path.Path(['abc', 'def']);
+    expect(path1.compareTo(path4)).toEqual(1);
+    expect(path4.compareTo(path5)).toEqual(0);
+    const path6 = new path.Path(['abc', 'def', 'ghi', 'klm']);
+    expect(path6.compareTo(path1)).toEqual(-1);
+    expect(path3.compareTo(path6)).toEqual(-1);
   });
 
   test('documentId', () => {
