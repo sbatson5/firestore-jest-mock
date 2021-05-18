eb77a48303579d298b5ba166b1c48479df5168b1
Daniel Miranda <dmiranda2791@gmail.com>
2021-05-18 08:05:41 -0600

    fix: export mockGoogleCloudFirestore on type def (#104)

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 export { FakeFirestore } from './mocks/firestore';
 export { FakeAuth } from './mocks/auth';
 export { mockFirebase } from './mocks/firebase';
+export { mockGoogleCloudFirestore } from './mocks/googleCloudFirestore';
